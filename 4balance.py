1322a9f315c60648914778ce66a60475c4736c94
ManjiriKinage <manjirikinage2005@gmail.com>
2025-12-31 19:43:12 +0530

    4

--- 4balance.py
+++ 4balance.py
@@ -7,7 +7,7 @@ print("Type END to stop\n")
 while True :
     entry = input("enter transaction : ")
     
-    if entry.upper == "END":
+    if entry.upper() == "END":
         break
     
     t_type ,amount =entry.split()
@@ -15,14 +15,19 @@ while True :
     
     if t_type == 'D' :
         balance += amount
+        print("Deposited:", amount)
         
     elif t_type =='W':
         if balance- amount < MIN_BALANCE:
             print("Withdrawal denied! Minimum balance must be 500")
         else :
             balance -= amount
+            print("Withdrawn:", amount)
     else:
         print("Invalid transaction")
+    
+    print("Current Balance =", balance)
+    print("----------------------")
         
 print("\nNet Account Balance =", balance)
 
